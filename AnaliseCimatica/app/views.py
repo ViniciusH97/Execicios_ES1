3d2678146d0bfa65c16573e67a9bee4cc7848be1
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2023-10-30 22:16:25 -0300

    Add files via upload

--- AnaliseCimatica/app/views.py
+++ AnaliseCimatica/app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 from . models import *
 
+def administracao(request):
+    administracao = {
+        'administracao':administracao.adm.all()
+    }
+    return render(request, 'administracao/adm.html'.adm)
+
+def continentes(request):
+    continentes = {
+        'continentes':continentes.adm.all()
+    }
+    return render(request, 'continentes/continentes.html'.continentes)
+
+def estados(request):
+    estados = {
+        'estados':estados.es.all()
+    }
+    return render(request, 'estados/estados.html'. estados)
