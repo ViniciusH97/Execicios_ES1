fc26fc4270d44715a681dbaf04b076bfdc994ecf
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2023-11-06 13:57:52 -0300

    Add files via upload

---
+++ AnaliseClimatica/app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from app.models import *
+
+admin.site.register(Continentes)
+admin.site.register(Pais)
+admin.site.register(Estados)
+admin.site.register(DadosClimaticos)
+admin.site.register(Temperatura)
