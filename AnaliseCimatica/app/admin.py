97e56ee48fbe5be4253967d92aaf33724fc967bf
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2023-11-06 13:58:14 -0300

    Delete AnaliseCimatica directory

--- AnaliseCimatica/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app.models import *
-
-admin.site.register(Continentes)
-admin.site.register(Pais)
-admin.site.register(Estados)
-admin.site.register(DadosClimaticos)
-admin.site.register(Temperatura)
