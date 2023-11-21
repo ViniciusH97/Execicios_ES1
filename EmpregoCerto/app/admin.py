8f2d0e2af090dc25e4f124ea2b3457011456c83a
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2023-11-21 16:24:09 -0300

    Delete EmpregoCerto directory

--- EmpregoCerto/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from app.models import *
-
-admin.site.register(Empresa)
-admin.site.register(Vaga)
-admin.site.register(Candidato)
-admin.site.register(Login)
-admin.site.register(Usuario)
-
