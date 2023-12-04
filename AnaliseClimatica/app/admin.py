609c9b6721f8311da583e93f4e05129404ba625e
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2023-12-04 15:44:25 -0300

    Add files via upload

---
+++ AnaliseClimatica/app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from app.models import *
+from app.forms import *
+
+admin.site.register(Estado)
+admin.site.register(Clima)
+admin.site.register(Continente)
+admin.site.register(Pais)
+admin.site.register(Causa)
---
+++ AnaliseClimatica/config/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+from app import views
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('adm', TemplateView.as_view(template_name='adm/adm.html'), name='adm'),
+    path('index', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('causa', TemplateView.as_view(template_name='causa/consultar_causa.html'), name='causa'),
+    path('buscar_causas', views.buscar_causas, name='buscar_causas'),
+
+]   
