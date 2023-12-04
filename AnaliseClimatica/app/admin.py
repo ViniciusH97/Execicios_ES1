60d491300cf4e6a876862ac95758299541f3900b
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2023-12-04 15:41:34 -0300

    Delete AnaliseClimatica directory

--- AnaliseClimatica/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from app.models import *
-from app.forms import *
-
-admin.site.register(Estado)
-admin.site.register(Clima)
-admin.site.register(Continente)
-admin.site.register(Pais)
-admin.site.register(Usuario)
