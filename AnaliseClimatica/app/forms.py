d3bdb5b3404ec38a35ac71d6e0614d45c6cf2428
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2023-12-10 15:56:49 -0300

    Delete AnaliseClimatica directory

--- AnaliseClimatica/app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import *
-
-class LoginForm(forms.Form):
-    username = forms.CharField(label='Usuário')
-    password = forms.CharField(label='Senha', widget=forms.PasswordInput)
-        
