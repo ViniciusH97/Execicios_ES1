9b7c3601bcaf59d307e884ecc55aa75544e73c05
Vinicius Lima <106411366+ViniciusH97@users.noreply.github.com>
2023-12-10 15:38:44 -0300

    Add files via upload

---
+++ AnaliseClimatica/app/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+class Pais(models.Model):
+    nome = models.CharField(max_length=40)
+    populacao = models.IntegerField()
+    area = models.IntegerField()
+    densidade = models.DecimalField(max_digits=4, decimal_places=2)
+    capital = models.CharField(max_length=40)
+    moeda = models.CharField(max_length=40)
+    idioma = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.nome
+
+class Continente(models.Model):
+    nome = models.CharField(max_length=40)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nome
+    
+class Clima(models.Model):
+    nome = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.nome
+    
+class Estado(models.Model):
+    nome = models.CharField(max_length=30)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    clima = models.ForeignKey(Clima, on_delete=models.CASCADE)
+    temperatura = models.DecimalField(max_digits=4, decimal_places=2)
+    
+    def __str__(self):
+        return self.nome
+
+class Causa(models.Model):
+    nome = models.CharField(max_length=40)
+    descricao = models.CharField(max_length=500)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    continente = models.ForeignKey(Continente, on_delete=models.CASCADE)
+    estado = models.ForeignKey(Estado, related_name='causas', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nome
---
+++ AnaliseClimatica/config/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+from app import views
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('adm', TemplateView.as_view(template_name='adm/adm.html'), name='adm'),
+    path('index', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('causa', TemplateView.as_view(template_name='causa/consultar_causa.html'), name='causa'),
+    path('buscar_causas', views.buscar_causas, name='buscar_causas'),
+    path('estado/', views.mostrar_dados, name='estado'),
+    path('index', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', views.mostrar_dados, name='index'),
+    path('paises/', views.consultar_paises, name='paises'),
+]   
